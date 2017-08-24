4e9c901c974a1d5fbe80456a48e340fc27638ece
Ilia K <ki.stfu@gmail.com>
2017-08-24 21:58:16 +0300

    ear: Initial commit

---
+++ src/ear/ear.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import codecs
+import optparse
+import subprocess
+import zmq
+
+def capture_frame(nsec):
+    arecord = subprocess.Popen(['arecord',
+                                '--file-type', 'raw',
+                                '--channels', '1',
+                                '--format', 'S16_LE',
+                                '--rate', '16000',
+                                '--duration', str(nsec)],
+                               stdout=subprocess.PIPE)
+    return arecord.communicate()[0]
+
+def main(options, args):
+    publish_to = options.ensure_value('publish_to', None)
+    control_by = options.ensure_value('control_by', None)
+    assert publish_to and control_by
+
+    context = zmq.Context()
+    pub_sock = context.socket(zmq.PUB)
+    pub_sock.bind(publish_to)
+    ctrl_sock = context.socket(zmq.PULL)
+    ctrl_sock.bind(control_by)
+    import time
+    while True:
+        print('Waiting...', flush=True)
+        time.sleep(1)
+        task = {'action' : 'capture', 'duration' : 5}
+        #task = ctrl_sock.recv_json()
+        print('Task: %s' % task, flush=True)
+        if task['action'] == 'capture':
+            frame = capture_frame(task['duration'])
+            pub_sock.send_json({'frame' : codecs.encode(frame, 'base64')})
+
+if __name__ == '__main__':
+    parser = optparse.OptionParser()
+    parser.add_option('--publish_to', dest='publish_to',
+                      help='Publish audio fragments to the specified address')
+    parser.add_option('--control_by', dest='control_by',
+                      help='Control the ear through this address')
+    main(*parser.parse_args())
