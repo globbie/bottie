c6d1a6aae4741233d81f1e57d795ef40c7b33911
Ilia K <ki.stfu@gmail.com>
2017-08-29 23:53:25 +0300

    ear: Use voice to activate ear_capture

--- src/ear/ear.py
+++ src/ear/ear.py
@@ -5,6 +5,21 @@ import optparse
 import subprocess
 import zmq
 
+import pyaudio
+import numpy as np
+
+CHUNKSIZE = 1024
+
+def wait_signal(audio):
+    stream = audio.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=CHUNKSIZE)
+    while True:
+        str_data = stream.read(CHUNKSIZE)
+        raw_data = numpy.fromstring(str_data, dtype=numpy.int16)
+        for i in raw_data:
+            if i > 30000:
+                return
+    stream.close()
+
 def capture_frame(nsec):
     arecord = subprocess.Popen(['arecord',
                                 '--file-type', 'raw',
@@ -17,17 +32,17 @@ def capture_frame(nsec):
 
 def main(options, args):
     publish_to = options.ensure_value('publish_to', None)
-    control_by = options.ensure_value('control_by', None)
-    assert publish_to and control_by
+    assert publish_to
+
+    audio = pyaudio.PyAudio()
 
     context = zmq.Context()
     pub_sock = context.socket(zmq.PUB)
     pub_sock.bind(publish_to)
-    ctrl_sock = context.socket(zmq.PULL)
-    ctrl_sock.bind(control_by)
     while True:
         print('Waiting...', flush=True)
-        task = ctrl_sock.recv_json()
+        wait_signal(audio)
+        task = {'action' : 'capture', 'duration' : 3}
         print('Task: %s' % task, flush=True)
         if task['action'] == 'capture':
             frame = capture_frame(task['duration'])
@@ -37,6 +52,4 @@ if __name__ == '__main__':
     parser = optparse.OptionParser()
     parser.add_option('--publish_to', dest='publish_to',
                       help='Publish audio fragments to the specified address')
-    parser.add_option('--control_by', dest='control_by',
-                      help='Control the ear through this address')
     main(*parser.parse_args())
