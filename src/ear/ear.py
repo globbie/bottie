77fea8c6900a3f17fbe277c6729c2e66fa50e165
Ilia K <ki.stfu@gmail.com>
2017-08-30 10:49:11 +0300

    ear: capture() uses pyaudio instead of subprocess&arecord

--- src/ear/ear.py
+++ src/ear/ear.py
@@ -1,51 +1,39 @@
 #!/usr/bin/env python3
 
 import codecs
+import numpy
 import optparse
-import subprocess
+import pyaudio
 import zmq
 
-import pyaudio
-import numpy as np
+BITRATE = 16000
 
-CHUNKSIZE = 1024
-
-def wait_signal(audio):
-    stream = audio.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=CHUNKSIZE)
+def wait_signal(stream):
     while True:
-        str_data = stream.read(CHUNKSIZE)
-        raw_data = numpy.fromstring(str_data, dtype=numpy.int16)
-        for i in raw_data:
-            if i > 30000:
-                return
-    stream.close()
+        raw_data = numpy.fromstring(stream.read(BITRATE // 4), dtype=numpy.int16)
+        if len([vol for vol in raw_data if vol > 30000]) > 0:
+            return
 
-def capture_frame(nsec):
-    arecord = subprocess.Popen(['arecord',
-                                '--file-type', 'raw',
-                                '--channels', '1',
-                                '--format', 'S16_LE',
-                                '--rate', '16000',
-                                '--duration', str(nsec)],
-                               stdout=subprocess.PIPE)
-    return arecord.communicate()[0]
+def capture_frame(stream, nsec):
+    return stream.read(BITRATE * nsec)
 
 def main(options, args):
     publish_to = options.ensure_value('publish_to', None)
     assert publish_to
 
     audio = pyaudio.PyAudio()
+    stream = audio.open(format=pyaudio.paInt16, channels=1, rate=BITRATE, input=True)
 
     context = zmq.Context()
     pub_sock = context.socket(zmq.PUB)
     pub_sock.bind(publish_to)
     while True:
         print('Waiting...', flush=True)
-        wait_signal(audio)
+        wait_signal(stream)
         task = {'action' : 'capture', 'duration' : 3}
         print('Task: %s' % task, flush=True)
         if task['action'] == 'capture':
-            frame = capture_frame(task['duration'])
+            frame = capture_frame(stream, task['duration'])
             pub_sock.send_json({'frame' : codecs.encode(frame, 'base64').decode('ascii')})
 
 if __name__ == '__main__':
